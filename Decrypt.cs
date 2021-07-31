b685b02510cd128af979ee84b55990aca27f4b87
minijamigara <55281954+minijamigara@users.noreply.github.com>
2021-07-31 18:38:27 +0530

    Add files via upload

---
+++ Decrypt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Decrypt
+{
+    class Decrypt
+    {
+        public static string DecryptString(string key, string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace Decrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedPath = "E:\\encryptedData.txt";
+            string keyPath = "E:\\key.txt";
+            string decryptedPath = "E:\\decryptedData.txt";
+
+            string[] files = new string[10];
+
+            //check if the files exists or not
+            if ((File.Exists(encryptedPath)) && (File.Exists(keyPath)))
+            {
+                long length = new System.IO.FileInfo(encryptedPath).Length;
+
+                //check if the file size is greater than 5mB or not
+                if (length <= 5000000)
+                {
+                    var str = File.ReadAllText(encryptedPath); // Read encrypted file data
+                    var key = File.ReadAllText(keyPath); //Read key file data
+
+                    /*
+                     * files[0] = key file
+                     * files[1] = original file
+                     * files[2] = encrypted file
+                     */
+
+                    //Remove the last two characters
+                    files[0] = key.Remove(key.Length - 2);
+                    files[1] = str.Remove(str.Length - 2);
+                    files[2] = Decrypt.DecryptString(files[0], files[1]); //key , cipher text
+
+                    if (File.Exists(decryptedPath))
+                    {
+                        File.Delete(decryptedPath);
+                        StreamWriter OurStream;
+                        OurStream = File.CreateText(decryptedPath);
+                        OurStream.WriteLine(files[2]);
+                        OurStream.Close();
+                        Console.WriteLine("Key :" + files[0]);
+                        Console.WriteLine("Decrypt string :" + files[2]);
+                        Console.WriteLine("Created Decrypted File!");
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine("Successfully Decrypted !!!", Console.ForegroundColor);
+                    }
+                    else
+                    {
+                        StreamWriter OurStream;
+                        OurStream = File.CreateText(decryptedPath);
+                        OurStream.WriteLine(files[2]);
+                        OurStream.Close();
+                        Console.WriteLine("Created Decrypted File!");
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine("Successfully Decrypted !!!", Console.ForegroundColor);
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("File size exceed !",Console.ForegroundColor);
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Cannot find the file", Console.ForegroundColor);
+            }
+        }
+    }
+}
